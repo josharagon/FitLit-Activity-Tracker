11390dadf5b99e7130f951573e087614fe8ac794
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-02-26 22:07:59 -0700

    Refactor chart compiler to work with all health categories

--- src/scripts.js
+++ src/scripts.js
@@ -89,22 +89,7 @@ function startApp() {
     const weekHydrationRecord = hydrationObject.hydrationData.filter(drink => drink.userID === hydrationObject.user.id);
     // hydrationThisWeek.insertAdjacentHTML('afterBegin', makeHydrationHTML(hydrationObject.user.id, hydrationObject, hydrationObject.user, hydrationObject.calculateFirstWeekOunces()));
     // hydrationEarlierWeek.insertAdjacentHTML('afterBegin', makeHydrationHTML(hydrationObject.user.id, hydrationObject, hydrationObject.user, hydrationObject.calculateRandomWeekOunces()));
-    compileHydrationChart(hydrationObject)
-  }
-
-  function compileHydrationChart(hydrationObject) {
-    console.log(hydrationObject.calculateFirstWeekOunces())
-    let hydrationChart = new JSC.Chart("chartDiv-hydration", {
-      type: 'spline',
-      legend_visible: false,
-      axisTick_gridline: {visible: false},
-      box_fill: '#5bc8ac',
-      series: [
-        {
-          points: hydrationObject.calculateFirstWeekOunces()
-        },
-      ]
-    });
+    compileChart(hydrationObject, "numOunces")
   }
 
   function makeHydrationHTML(id, hydrationInfo, userStorage, drinks) {
@@ -147,16 +132,16 @@ function startApp() {
     avgStepsToday.insertAdjacentHTML("afterBegin", `<p>Step Count:</p><p>All Users</p><p><span class="number">${averageSteps}</span></p>`)
     // average number of steps for everyone today
     //weekly views:
-    compileActivityChart(activityRepo, "numSteps")
-    compileActivityChart(activityRepo, "flightsOfStairs")
-    compileActivityChart(activityRepo, "minutesActive")
+    compileChart(activityRepo, "numSteps")
+    compileChart(activityRepo, "flightsOfStairs")
+    compileChart(activityRepo, "minutesActive")
     // userStepsThisWeek.insertAdjacentHTML("afterBegin", makeStepsHTML(activityRepo.userDataForWeek("numSteps")));
     //console.log(activityRepo.userDataForWeek("minutesActive"));
     //console.log(activityRepo.userDataForWeek("flightsOfStairs"));
 
   }
 
-  function compileActivityChart(activityRepo, activityType) {
+  function compileChart(activityRepo, activityType) {
     let chart = new JSC.Chart(`chartDiv-${activityType}`, {
       type: 'spline',
       legend_visible: false,
